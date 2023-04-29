59252e688ce1f4ce28305158b063d876fcff6806
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2023-04-29 09:25:55 -0500

    fix formatting

--- point_cloud_registration/register.py
+++ point_cloud_registration/register.py
@@ -44,9 +44,10 @@ canonicals = np.array([
         [ 0.109,  0.249,  0.27 ],
         [ 0.185,  0.166,  0.288]
     ]
-])
+])[:4]   # use only four features
+
 canonicals -= canonicals.mean(axis=1, keepdims=True)
-canonicals /= np.linalg.norm(canonicals, axis=(1,2), keepdims=True)
+canonicals /= np.linalg.norm(canonicals, axis=(1, 2), keepdims=True)
 
 
 def make_tetras(points):
@@ -54,25 +55,41 @@ def make_tetras(points):
     :param np.array points: n x 3 point cloud
     :return np.array tetras: m x 4 indices of compact tetrahedrons
     """
+    # compute max tetrahedron diameter
     RELATIVE_RADIUS = 1.3
     CUTOFF_QUANTILE = 0.10
-    
     tree = spatial.KDTree(points)
-
-    # compute search radius
     d4, _ = tree.query(points, [4])
-    r = RELATIVE_RADIUS * np.quantile(d4, CUTOFF_QUANTILE)
+    max_diameter = RELATIVE_RADIUS * np.quantile(d4, CUTOFF_QUANTILE)
 
+    # Generate all tetrahedrons below max_diameter 
     return np.array(list(set(
         itertools.chain.from_iterable(
             itertools.combinations(x, 4) 
-            for x in tree.query_ball_point(points, r) 
+            for x in tree.query_ball_point(points, max_diameter) 
             if len(x) >=4))))
 
 
 def ortho_procrustes(A, B):
     U, D, V = np.linalg.svd(A.T @ B)
     return U @ V, D.sum()
+
+
+def fit(A, B):
+    """
+    Least-squares best-fit transform that maps points A into points B
+    :param A: Nxm numpy array of corresponding points
+    :param B: Nxm numpy array of corresponding points
+    :return: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
+    """
+    m = A.shape[1]  # space dimensions 
+    centroid_A = A.mean(axis=0)
+    centroid_B = B.mean(axis=0)
+    R, _ = ortho_procrustes(A - centroid_A, B - centroid_B)
+    T = np.identity(m + 1)
+    T[:m, :m] = R.T
+    T[:m, m] = centroid_B.T - R.T @ centroid_A.T
+    return T
 
 
 def disparity(A, B):
@@ -84,9 +101,16 @@ def disparity(A, B):
     return np.square(A - scale * B @ R.T).sum()
 
 
+def align(A, B):
+    """
+    
+    reorder vertices in A for mininm
+    """
+
 def min_disparity(A, B):
     """
-    same as disparity(A, B) except the tetra is assumed unordered
+    :params A, B: tetrahedrons
+    {returns: disparity(A, B) minimized over all vertex orderings  
     """
     return min(
         disparity(np.array(t), B) 
@@ -94,6 +118,11 @@ def min_disparity(A, B):
 
 
 def make_normal_tetras(points):
+    """
+    :param np.array points: n x 3 point coordinates
+    :return: dict with fields: coords, means, norms, and norm_coords 
+    describing m local tetrahedrons
+    """
     vertices = make_tetras(points)
     coords = points[vertices, :]
     means = coords.mean(axis=1, keepdims=True)
@@ -132,9 +161,11 @@ def remove_common_tetras(tetras, max_density_threshold=0.2):
 
 def match_features(tetras1, tetras2):
     tree = spatial.KDTree(tetras1['features'])
-    d4, _ = tree.query(tetras2['features'], [1])
+    d, _ = tree.query(tetras2['features'], [1])
     distances, matches = tree.query(
         tetras2['features'], [1],
-        distance_upper_bound=1.1 * np.quantile(d4, 0.2))
+        distance_upper_bound=1.1 * np.quantile(d, 0.2))
     distances = distances[:,0]
     return distances, matches
+
+
